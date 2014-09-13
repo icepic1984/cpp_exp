447384377203522ed628f6ab181e2967b3d4dd8c
icepic <icepic2009@googlemail.com>
2014-09-13 17:08:04 +0200

    cleanup tuple

--- tuple.cpp
+++ tuple.cpp
@@ -57,34 +57,12 @@ private:
    
 };
 
-template<typename T>
-struct identity 
-{
-   typedef T type;
-};
-
-
-template<typename T>
-void meow(T t, 
-		  typename identity<std::function<void (T)>>::type f)
-{f(t);}
 
 int main(int argc, char *argv[])
 {
-	
-	std::function<void (int)> fxn = [](int a)
-	   {
-		   std::cout << a << std::endl;
-	   };
-
-	meow(3,fxn);
-	meow(4, [](int a){std::cout<<a<<std::endl;});
-	
-	 tuple<int,std::string> t;
-	 get<0>(t);
-	// get<1>(t) = std::string("hallo");
-	
-	// std::cout << get<1>(t)<< std::endl;
-	
+	tuple<int,std::string> t;
+	 get<0>(t) = 10;
+	 get<1>(t) = std::string("hallo");
+	 std::cout << get<1>(t)<< std::endl;
     return 0;
 }
