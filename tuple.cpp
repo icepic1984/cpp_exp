b155cb35787ba37778a28d2c11ae5fea8c76bb06
icepic <icepic2009@googlemail.com>
2014-09-14 10:29:25 +0200

    Smarttabs

--- tuple.cpp
+++ tuple.cpp
@@ -25,13 +25,13 @@ struct tuple_element<k, tuple<T, Ts...>>
 
 template<size_t k, typename... Ts>
 typename std::enable_if<k == 0, 
-						typename tuple_element<0,tuple<Ts...>>::type&>::type
+                        typename tuple_element<0,tuple<Ts...>>::type&>::type
    get(tuple<Ts...>& t)
 {return t.head_;}
 
 template<size_t k, typename T, typename... Ts>
 typename std::enable_if<k != 0,
-						typename tuple_element<k,tuple<T,Ts...>>::type&>::type
+                        typename tuple_element<k,tuple<T,Ts...>>::type&>::type
    get(tuple<T,Ts...>& t)
 {
 	tuple<Ts...> & super = t;
@@ -44,25 +44,23 @@ class tuple<T,Ts...> : private tuple<Ts...>
 public:
    template<size_t k, typename... T2s>
    friend typename std::enable_if<k == 0, 
-						typename tuple_element<0,tuple<T2s...>>::type&>::type
+                                  typename tuple_element<0,tuple<T2s...>>::type&>::type
 	  get(tuple<T2s...>& t);
 
    template<size_t k, typename T1, typename... T2s>
    friend typename std::enable_if<k != 0,
-						   typename tuple_element<k,tuple<T1,T2s...>>::type&>::type
-	  get(tuple<T1,T2s...>& t);
-   
+                                  typename tuple_element<k,tuple<T1,T2s...>>::type&>::type
+      get(tuple<T1,T2s...>& t);
 private:
    T head_;
-   
 };
 
 
 int main(int argc, char *argv[])
 {
 	tuple<int,std::string> t;
-	 get<0>(t) = 10;
-	 get<1>(t) = std::string("hallo");
-	 std::cout << get<1>(t)<< std::endl;
+	get<0>(t) = 10;
+	get<1>(t) = std::string("hallo");
+	std::cout << get<1>(t)<< std::endl;
     return 0;
 }
