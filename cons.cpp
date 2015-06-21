a8e429aab9a754a2d491015d3b8d5c9810f84c7d
icepic <icepic2009@googlemail.com>
2015-06-21 14:57:30 +0200

    Sort array during compile time

--- cons.cpp
+++ cons.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <functional>
 #include <string>
 
 // Define nil
@@ -47,17 +48,29 @@ constexpr const tail_<L>& tail(const L& l)
 
 // Calculate size
 template <class List>
-constexpr unsigned size(List const& l) { return 0; }
+constexpr std::size_t size(List const& l) { return 0; }
  
 template <class H, class T>
-constexpr unsigned size(cons_<H,T> const& l) { return 1 + size(l.tail); }
+constexpr std::size_t size(cons_<H,T> const& l) { return 1 + size(l.tail); }
+
+template <typename List>
+struct size_
+{
+   static constexpr std::size_t value = 1 + size_<typename List::tail_>::value;
+};
+
+template <>
+struct size_<nil_>
+{
+   static constexpr std::size_t value = 0;
+};
 
 // Helper to create list
-constexpr auto list() -> decltype(nil)
+constexpr auto list() -> decltype(auto)
 {return nil;}
 
 template <typename T>
-constexpr auto list(T x) -> decltype(cons(x,nil))
+constexpr auto list(T x) -> decltype(auto)
 {return cons(x,nil);}
 
 // Create list with variadic elements
@@ -79,7 +92,7 @@ struct list_t<H>
 
 // Implementation
 template <typename H, typename ...T>
-constexpr auto list(H h, T ...args) -> typename list_t<H,T...>::type
+constexpr auto list(H h, T ...args) -> decltype(auto)
 {return cons(h,list(args...));}
 
 // Print
@@ -109,7 +122,7 @@ std::ostream& operator<<(std::ostream& s, const cons_<H,T>& x)
 template <std::size_t N, typename List> struct split_;
 
 template <std::size_t N, typename List>
-constexpr auto split(const List& x) -> decltype(split_<N,List>::call(x))
+constexpr auto split(const List& x) -> decltype(auto)
 {return split_<N,List>::call(x);}
 
 template <std::size_t N, typename List>
@@ -133,27 +146,159 @@ template <typename List>
 struct split_<0,List>
 {
    static constexpr auto
-   call(const List& l) -> decltype(cons(nil,l)){
+   call(const List& l) -> decltype(auto){
 	   return cons(nil,l);
    }
 };
 
 
+template <typename X, typename Y>
+constexpr bool less(const X& x, const Y& y) 
+{return x < y;}
+
+template <typename L1, typename L2> struct merge_;
+
+template <typename L1, typename L2>
+constexpr auto merge(const L1& a, const L2& b) -> decltype(auto)
+{return merge_<L1,L2>::call(a,b);}
+
+template <typename L1, typename L2>
+struct merge_
+{
+  static constexpr auto
+  call(const L1& a, const L2& b) -> decltype(auto){
+	  return (less(head(a),head(b))) ?
+	     cons(head(a),merge(tail(a),b)) :
+	     cons(head(b),merge(a, tail(b)));
+  }
+};
+
+template <typename L1>
+struct merge_<L1,nil_> 
+{
+   static constexpr auto
+   call(const L1& a, const nil_& n) -> decltype(auto){
+	   return a;
+   }
+};
+
+template <typename L2>
+struct merge_<nil_,L2> 
+{
+   static constexpr auto
+   call(const nil_& n, const L2& b) -> decltype(auto){
+	   return b;
+   }
+};
+
+template <>
+struct merge_<nil_,nil_> 
+{
+   static constexpr auto
+   call(const nil_& n1, const nil_& n2) -> decltype(auto){
+	   return n1;
+   }
+};
+
+
+template <typename List, bool stop = (size_<List>::value <=1)>
+struct sort_;
+
+template <typename List>
+constexpr auto sort(const List& l) -> decltype(auto)
+{return sort_<List>::call(l);}
+
+template <typename List>
+constexpr auto sort2(const List& l) -> decltype(auto)
+{
+	return (size(l) <= 1) ?  l :
+	   merge(sort2(head(split<(size_<List>::value / 2)>(l))),
+	         sort2(tail(split<(size_<List>::value / 2)>(l))));
+}
+
+template <typename List, bool stop>
+struct sort_
+{
+  static constexpr auto
+  call(const List& l) -> decltype(auto) {
+	  return merge(sort(head(split<(size_<List>::value / 2)>(l))),
+	               sort(tail(split<(size_<List>::value / 2)>(l))));
+  } 
+
+};
+
+template <typename List>
+struct sort_<List,true>
+{
+  static constexpr auto
+  call(const List& l) -> decltype(auto) {
+	  return l;
+  } 
+
+};
+
+template <std::size_t N,typename List>
+struct get_;
+
+template <std::size_t N,typename List>
+constexpr auto get(const List& l) -> decltype(auto)
+{return get_<N,List>::call(l);} 
+
+template<std::size_t N, typename List>
+struct get_
+{
+   static constexpr auto
+   call(const List& l) -> decltype(auto) {
+	   return get<(N-1)>(tail(l));
+   }
+};
+
+template<typename List>
+struct get_<1,List>
+{
+   static constexpr auto
+   call(const List& l) -> decltype(auto){
+	   return head(l);
+   }
+};
+
+
+template <typename F, typename List>
+struct apply_;
+
+template <typename F, typename List>
+constexpr auto apply(const List& l, F f) -> decltype(auto)
+{
+	return apply_<F,List>::call(l,f);
+}
+
+template <typename F, typename List>
+struct apply_ 
+{
+   static constexpr auto
+   call(const List& l, F f){
+	   return cons(f(head(l)),apply(tail(l),f));
+   }
+};
+
+template <typename F>
+struct apply_<F,nil_>
+{
+   static constexpr auto
+   call(const nil_& n, F f){
+	   return n;
+   }
+};
+
+constexpr double add(double x)
+{return x+100;}
+
+constexpr auto l1 = sort(list(12.4,12.3,302.0,10.0,99.0));
+constexpr auto l2 = apply(l1,add);
+
 int main(int argc, char *argv[])
 {
-	
-	auto bla = list(list(10,std::string("erer"),10),10);
-	auto blup = cons(10,cons(20,10));
-
-	auto test = cons(10,cons(20,cons(40,cons(50,nil))));
-	auto sp = split<1>(test);
-	
-	std::cout << test << std::endl;
-	std::cout << sp << std::endl;
-	std::cout << size(bla) << std::endl;
-	std::cout << bla << std::endl;
-	std::cout << blup << std::endl;
-	
+	std::cout << l2 << std::endl;
 	return 0;
 	
 }
--- constexpr.cpp
+++ constexpr.cpp
@@ -27,7 +27,7 @@ constexpr auto generate(std::index_sequence<Index...>) ->
 }
 	
 template <std::size_t N1, std::size_t N2, std::size_t... I1, std::size_t... I2>
-constexpr auto merge(const array<N1>& a, const array<N2>& b,
+constexpr auto cons(const array<N1>& a, const array<N2>& b,
                      std::index_sequence<I1...>,
                      std::index_sequence<I2...>) ->
 	decltype(array<N1 + N2> {})
@@ -37,11 +37,41 @@ constexpr auto merge(const array<N1>& a, const array<N2>& b,
 
 
 template <std::size_t N1, std::size_t N2>
-constexpr auto merge(const array<N1>& a, const array<N2>& b) ->
+constexpr auto cons(const array<N1>& a, const array<N2>& b) ->
 	decltype(array<N1+N2>{})
 {
-	return merge(a,b,std::make_index_sequence<N1>{},
+	return cons(a,b,
+	             std::make_index_sequence<N1>{},
 	             std::make_index_sequence<N2>{});
+}
+
+template <std::size_t N2>
+constexpr auto cons(const std::size_t a, const array<N2>& b) ->
+	decltype(array<N2+1>{})
+{
+	return cons(array<1>{{a}},
+	             b,
+	             std::make_index_sequence<1>{},
+	             std::make_index_sequence<N2>{});
+}
+
+template <std::size_t N1>
+constexpr auto cons(const array<N1>& a, std::size_t b) ->
+	decltype(array<N1+1>{})
+{
+	return cons(a,
+	             array<1>{b},
+	             std::make_index_sequence<N1>{},
+	             std::make_index_sequence<1>{});
+}
+
+constexpr auto cons(std::size_t a, std::size_t b) ->
+	decltype(array<2>{})
+{
+	return cons(array<1>{a},
+	             array<1>{b},
+	             std::make_index_sequence<1>{},
+	             std::make_index_sequence<1>{});
 }
 
 template <std::size_t N, std::size_t... I1, std::size_t... I2>
@@ -65,8 +95,8 @@ constexpr auto split(const array<N>& a) ->
 
 template<std::size_t N>
 constexpr auto head(const array<N>& a) ->
-	decltype(array<1> {})
-{return {{a[0]}};}
+	decltype(auto)
+{return a[0];}
 
 template<std::size_t N, std::size_t... Is>
 constexpr auto tail(const array<N>& a, std::index_sequence<Is...>) ->
@@ -78,17 +108,92 @@ constexpr auto tail(const array<N>& a) ->
 	decltype(array<N-1>{})
 {return tail(a,make_index_range<1,N> {});}
 	            
+template <std::size_t N1, std::size_t N2>
+struct merge_;
 
-constexpr array<4> a1  {{10,5,2,6}};
-constexpr array<4> a2  {{8,3,5,2}};	
-constexpr auto m1 = merge(a1,a2);
+template <std::size_t N1, std::size_t N2>
+constexpr auto merge(const array<N1>& a, const array<N2>& b)
+	-> decltype(auto)
+{
+	return merge_<N1,N2>::call(a,b);
+}
 
+template <typename X, typename Y>
+constexpr bool less(const X& x, const Y& y)
+{return x < y;}
+
+template <std::size_t N1, std::size_t N2>
+struct merge_
+{
+   static constexpr auto
+   call(const array<N1>& a, const array<N2>& b) -> decltype(auto){
+	   return (less(head(a), head(b))) ?
+	      cons(head(a),merge(tail(a),b)) :
+	      cons(head(b),merge(a,tail(b)));
+   }
+};
+
+template <std::size_t N2>
+struct merge_<0,N2>
+{
+   static constexpr auto
+   call(const array<0>& a, const array<N2>& b) -> decltype(auto){
+	   return b;
+   }
+};
+
+template <std::size_t N1>
+struct merge_<N1,0>
+{
+   static constexpr auto
+   call(const array<N1>& a, const array<0>& b) -> decltype(auto){
+	   return a;
+   }
+};
+
+template <std::size_t N>
+struct sort_;
+
+template <std::size_t N>
+constexpr auto sort(const array<N>& l) -> decltype(auto)
+{return sort_<N>::call(l);}
+
+template <std::size_t N>
+struct sort_
+{
+  static constexpr auto
+  call(const array<N>& l) -> decltype(auto) {
+	  return merge(sort(split(l).first),
+	                    sort(split(l).second));
+  } 
+
+};
+
+template <>
+struct sort_<1>
+{
+  static constexpr auto
+  call(const array<1>& l) -> decltype(auto) {
+	  return l;
+  } 
+};
+
+constexpr array<12> a1  {{10,5,2,6,10,5,2,6,10,5,2,6}};
+constexpr array<12> a2  {{10,5,2,6,10,5,2,6,10,5,2,6}};
+constexpr auto s = sort(a2);
+
+template <typename T, std::size_t N>
+void test(std::array<T,N>&a ) 
+{std::cout << a[0] << std::endl;}
 
 int main()
 {
-	for(auto iter : m1) {
-		std::cout << iter << std::endl;
-	}
+	std::array<double,4> a{{3,4,5,6}};
+	test(a);
+	//std::cout << t << std::endl;
+	 for(auto iter : s) {
+	 	std::cout << iter << std::endl;
+	 }
 	 
 }
 
